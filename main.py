73f132ad80e0e8127540687f88de6dc3ccee8ab5
Ilmal Sabda Fathir <isaffathir@outlook.com>
2023-01-09 03:07:12 +0700

    No need hotreload from kivymd
    Already using kvhot library

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from kivymd.tools.hotreload.app import MDApp
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 
 from screens.screens import *
@@ -16,7 +16,7 @@ class NamaProjek(MDApp):
     KV_FILES = [
         'kv/home.kv',
     ]
-    def build_app(self):
+    def build(self):
         self.wm = WindowManager()
         screens = [
             Home(name="home"),
